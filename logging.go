60afed3ecb6d6ee8d599a3ed52dd1ade8fa640ae
Cenk Alti <cenkalti@gmail.com>
2014-03-04 15:26:08 -0800

    add comments to LogRecord fields

--- logging.go
+++ logging.go
@@ -120,15 +120,15 @@ type Handler interface {
 
 // Record contains all of the information about a single log message.
 type Record struct {
-	Format      string
-	Args        []interface{}
-	LoggerName  string
-	Level       Level
-	Time        time.Time
-	Filename    string
-	Line        int
-	ProcessID   int
-	ProcessName string
+	Format      string        // Format string
+	Args        []interface{} // Arguments to format string
+	LoggerName  string        // Name of the logger module
+	Level       Level         // Level of the record
+	Time        time.Time     // Time of the record (local time)
+	Filename    string        // File name of the log call (absolute path)
+	Line        int           // Line number in file
+	ProcessID   int           // PID
+	ProcessName string        // Name of the process
 }
 
 // Formatter formats a record.
